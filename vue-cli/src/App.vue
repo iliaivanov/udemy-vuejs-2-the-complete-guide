d2c32f274f61142b40f7c6cb1332a05bfefa8350
Ilia I <ilia.ivanov@bigbank.ee>
2019-05-09 13:35:55 +0300

    Simple webpack project generated by vue cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Hello world!</h1>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
